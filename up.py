1ad996f794c4edfa3f743ac81225daf7ab45ac6f
samcore <67498935+samcore07@users.noreply.github.com>
2026-08-20 20:24:05 +0530

    Delete up.py

--- up.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import upload_file
-
-upload_file(
-    path_or_fileobj="skin_disease_model_final.pth",  # Local file path
-    path_in_repo="skin_disease_model_final.pth",     # Where it will sit inside the repo
-    repo_id="samlowkey/skin-disease",
-    repo_type="model"
-)
